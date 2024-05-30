d7e7285b46c406ab6c76678019b60273347b00f0
restrepoospino <restrepoospino@gmail.com>
2024-05-30 10:10:56 -0500

    Segundo commit, se añade la clase donde se almacena la información de la tabla RegistroUsuario, la cual permite guardarlos para poder listarlos en la interfaz.

---
+++ OneDrive/Documentos/NetBeansProjects/DentalCare/src/main/java/com/carlosdevs/dentalcare/ListarRegistroUsuario.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.carlosdevs.dentalcare;
+
+
+public class ListarRegistroUsuario {
+    
+    // Variables para almacenar los datos que se van a listar.
+    String idRegistro;
+    String NombreUsuario;
+    String Email;
+    String Telefono;
+    String Contraseña;
+    String Genero;
+    
+    // Se guarda cada dato que se desea listar, para almacenarlos y mostrarlos en la interfaz.
+    
+    public String getIdRegistro() {
+        return idRegistro;
+    }
+
+    public void setIdRegistro(String idRegistro) {
+        this.idRegistro = idRegistro;
+    }
+
+    public String getNombreUsuario() {
+        return NombreUsuario;
+    }
+
+    public void setNombreUsuario(String NombreUsuario) {
+        this.NombreUsuario = NombreUsuario;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+    public String getContraseña() {
+        return Contraseña;
+    }
+
+    public void setContraseña(String Contraseña) {
+        this.Contraseña = Contraseña;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public void setGenero(String Genero) {
+        this.Genero = Genero;
+    }
+
+    boolean next() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    String getString(String idRegistro) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
